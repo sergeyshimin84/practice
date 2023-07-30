89664ba6e79c369653f6091dabe2d6464038d2d1
Sergey Shimin <sergey.shimin84@gmail.com>
2023-07-30 21:44:53 +0700

    finish

--- Snake/js/game.js
+++ Snake/js/game.js
@@ -28,23 +28,84 @@ snake[0] = {
     x: 9 * box,
     y: 10 * box
 };
+// Прослушиваем событие keydown всего документа
+document.addEventListener("keydown", direction);
+// Объявили пустую переменную dir
+let dir;
+// Указываем при нажатии каких клавиш какое событие передавать
+function direction (event) {
+    // В условии прописываем проверку на взаимоисключающие действия
+    if (event.keyCode == 37 && dir != "right")
+        dir = "left";
+    else if(event.keyCode == 38 && dir != "down")
+        dir = "up";
+    else if(event.keyCode == 39 && dir != "left")
+        dir = "right";
+    else if(event.keyCode == 40 && dir != "up")
+        dir = "down";            
+}
+// Создаем функцию остановке игры при совпадении координат головы и хвоста
+function eatTail(head, arr) {
+    for(let i = 0; i < arr.length; i++) {
+        if(head.x == arr[i].x && head.y == arr[i].y)
+        clearInterval(game);
+    }
+}
+
 // Отрисовываем объекты на игровом поле с помощью функции drawGame
-drawGame = () => {
+function drawGame () {
     // Указфваем переменную содержащую изобрадение и координаты
     ctx.drawImage(ground, 0, 0);
     // Получаем случайные координаты от food
     ctx.drawImage(foodImg, food.x, food.y);
     // Для отрисовки змеи задаем цикл
     for(let i = 0; i < snake.length; i++) {
-        // указываем цвет с помощью fillStyle
-        ctx.fillStyle = "red";
+        // указываем цвет с помощью fillStyle (голова останется зеленой, тело будет красным)
+        ctx.fillStyle = i == 0 ? "green" : "red";
         // указываем координаты змеи с шириной и высотой квадрата
         ctx.fillRect(snake[i].x, snake[i].y, box, box);
     }
+    // Прописываем счетчик счета
+    ctx.fillStyle = "white";
+    ctx.font = "50px Arial";
+    ctx.fillText(score, box * 2.2, box * 1.7);
+    // Указываем координаты переменных X и Y
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+   
+    if (snakeX == food.x && snakeY == food.y) {
+        score++;
+        food = {
+            x: Math.floor((Math.random() * 17 + 1)) * box,
+            y: Math.floor((Math.random() * 15 + 3)) * box,
+        };
+    } else {
+        snake.pop();
+    }
+    // Завершаем игру в случае выхода из игрового поля
+    if(snakeX < box || snakeX > box * 17
+        || snakeY < 3 * box || snakeY > box * 17)
+        clearInterval(game);
 
+    // Меняем координаты в зависимости от переданного dir
+    if(dir == "left") snakeX -= box;
+    if(dir == "right") snakeX += box;
+    if(dir == "up") snakeY -= box;
+    if(dir == "down") snakeY += box;
+    // "Новая голова", объект содержит в себе координаты змейки
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    // Объявляем функцию eatTail, передаем координаты головы и все элементы змейки
+    eatTail(newHead, snake);
+
+    snake.unshift(newHead);
 }
 
+
+
 // С помощью setInterval задаем частоту обновления картинки (частоту вызова функции drawGame)
-let game = setInterval(drawGame, 100);
+let game = setInterval(drawGame, 150);
 
 
